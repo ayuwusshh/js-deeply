c30dc6ce632b0a5c0da2726c93cc340176b9cb65
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-07-04 23:38:57 +0530

    2nd lec done

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId=54646;
+let email='ayush@example.com';
+var password='12345678';  //avoided
+accountCity="jaipur";
+
+
+// //accountId=54; not allowed
+// console.log(accountId); 
+
+// email='ayush@example.com';  //alowed
+
+// console.log(email);password=`455` // allowed
+// console.log(password);
+
+// accountCity='jar'; // allowed
+// console.log(accountCity); // allowed
+
+
+console.table({accountId, email, password, accountCity});
