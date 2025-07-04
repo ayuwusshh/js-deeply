3929515ea18612eec5c13d980984d4dae873e4dc
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-07-05 00:33:15 +0530

    conversion part 1 done

---
+++ basics/conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33a";
+console.log(typeof score); // string
+console.log(typeof (score)); // number
+
+let valNu=Number(score);
+console.log(typeof valNu);
+console.log(valNu); // NaN
+
+
+
+// "33"=> 33 type=> number
+// "33a"=> NaN type=> number
+// true => 1; false => 0  type=> number
+
+
+// Boolean
+// 1=>true;0=>false
+// ""=>false;"any string"=>true
