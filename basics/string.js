19b672e6898442c1138cfa369a86af411a5823d5
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-07-05 22:06:49 +0530

    string basics

---
+++ basics/string.js
@@ -0,0 +1,11 @@
+const name = "ayush"
+const repoCount = 50
+
+
+// console.log(name + repoCCount +"Value"); not recommended-
+
+console.log(`My name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName=new String("Ayush");
+console.log(gameName.__proto__);
+  
