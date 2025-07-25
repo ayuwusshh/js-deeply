067e6f300141026e6cdb4053c00e6fc24394962e
AYUSH KUMAR SINGH <145869549+ayuwusshh@users.noreply.github.com>
2025-07-25 16:22:07 +0530

    basic dom completed

---
+++ iterations/filterMap.js
@@ -0,0 +1,13 @@
+// const code = ['js', 'ruby', 'java', 'py']
+// const values=code.forEach((item)=>{
+// console.log(item);
+// return item
+// })
+// console.log(values);
+
+
+const myNuma = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNum = myNuma.filter((num) => {
+  return num > 4
+})
+console.log(newNum);
